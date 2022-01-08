1c310905be9c692c082c4f44e79800a776cc7955
Korgehah <korgehah@gmail.com>
2022-01-08 10:34:49 +0300

    make features

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,15 @@ import ozan from './assets/images/ozan.svg';
 import padoq from './assets/images/padoq.svg';
 import stockstotrade from './assets/images/stockstotrade.svg';
 import blank from './assets/images/blank.svg';
+import feature_1 from './assets/images/feature_1.png';
+import feature_2 from './assets/images/feature_2.png';
+import feature_3 from './assets/images/feature_3.png';
+import feature_4 from './assets/images/feature_4.png';
+import rank from './assets/images/rank.svg';
 /* components */
 import Button from './components/Button';
 import Navigation from './components/Navigation';
+import FeatureItems from './components/FeatureItems';
 
 const data = {
   headerNav: [
@@ -21,6 +27,29 @@ const data = {
     { name: 'Features', href: '' },
     { name: 'Roadmap', href: '' },
     { name: 'Docs', href: '' },
+  ],
+  features: [
+    {
+      title: 'Fix bugs 10x faster than competitors.',
+      subtitle: 'Let users pinpoint exactly where it happened.',
+      src: feature_1,
+    },
+    {
+      title: 'Replay bugs exactly as they happened.',
+      subtitle: 'See things through the eyes of your users.',
+      src: feature_2,
+    },
+    {
+      title: 'User feedback surveys.',
+      subtitle:
+        'Get user insights based on targeted surveys directly integrated in your app or website.',
+      src: feature_3,
+    },
+    {
+      title: 'Let users know they have been heard.',
+      subtitle: 'Build trust and relationships with your users.',
+      src: feature_4,
+    },
   ],
 };
 
@@ -34,7 +63,7 @@ const Header = ({ headerNav }: HeaderProps) => {
       <div className='wrapper header__wrapper'>
         <img className='header__logo' src={logo} alt='logo' />
         <Navigation navItems={headerNav} className='header__navigation' />
-        <a className='button-wrapper header__button-wrapper' href='vk.com'>
+        <a className='button-wrapper header__button-wrapper' href='zaglushka'>
           <Button className='header__button' buttonVariant='--light'>
             Sign in
           </Button>
@@ -48,7 +77,7 @@ const Promo = () => {
   return (
     <section className='promo'>
       <div className='wrapper promo__wrapper'>
-        <p className='promo__slogan'>
+        <p className='review promo__review'>
           “A winning and indispensible combination for the elimination of errors
           from your code.”
         </p>
@@ -59,7 +88,7 @@ const Promo = () => {
           Communicate with your users directly and build better software by
           discovering their everyday pain points.
         </p>
-        <a className='button-wrapper promo__button-wrapper' href='vk.com'>
+        <a className='button-wrapper promo__button-wrapper' href='zaglushka'>
           <Button className='promo__button' buttonVariant='--blue'>
             Get started for free
           </Button>
@@ -67,9 +96,7 @@ const Promo = () => {
         <div className='promo__example-wrapper'>
           <img className='promo__example' src={example} alt='example' />
         </div>
-        <p className='promo__partnership'>
-          Over 200 teams worldwide rely on Gleap
-        </p>
+        <p className='ads promo__ads'>Over 200 teams worldwide rely on Gleap</p>
         <div className='promo__partners'>
           <img
             className='promo__partners-item'
@@ -81,10 +108,39 @@ const Promo = () => {
           <img
             className='promo__partners-item'
             src={stockstotrade}
-            alt='scokstotrade'
+            alt='stockstotrade'
           />
           <img className='promo__partners-item' src={blank} alt='blank' />
         </div>
+      </div>
+    </section>
+  );
+};
+
+interface FeatureProps {
+  features: { title: string; subtitle: string; src?: string }[];
+}
+
+const Features = ({ features }: FeatureProps) => {
+  return (
+    <section className='features'>
+      <div className='wrapper features__wrapper'>
+        <FeatureItems features={features} />
+        <div className='ads features__ads'>
+          Integrate easily in minutes on mobile and web apps
+        </div>
+        <a className='button-wrapper features__button-wrapper' href='zaglushka'>
+          <Button className='features__button' buttonVariant='--blue'>
+            Get started for free
+          </Button>
+        </a>
+        <div className='features__rank-container'>
+          <img className='features__rank' src={rank} />
+        </div>
+        <p className='review features__review'>
+          “Gleap has delivered more than expected! It was implemented within
+          minutes and it is now an essential part of our development process.”
+        </p>
       </div>
     </section>
   );
@@ -96,6 +152,7 @@ function App() {
       <Header headerNav={data.headerNav} />
       <main className='main'>
         <Promo />
+        <Features features={data.features} />
       </main>
     </div>
   );
