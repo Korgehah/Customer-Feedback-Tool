aef5f10db11a5e1c4ec3ca72b72222a03140275c
Korgehah <korgehah@gmail.com>
2022-01-12 00:37:28 +0300

    make adaptive

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -3,27 +3,33 @@ import React from 'react';
 interface NavigationItemProps {
   name: string;
   href: string;
-};
-  
-const NavigationItem = ({name, href}: NavigationItemProps) => {
+}
+
+const NavigationItem = ({ name, href }: NavigationItemProps) => {
   return (
     <li className='navigation__item-wrapper'>
-      <a className='navigation__item' href={href}>{name}</a>
-      </li>
+      <a className='navigation__item' href={href}>
+        {name}
+      </a>
+    </li>
   );
 };
 
 interface NavigationProps {
   className: string;
-  navItems: {name: string; href: string;}[];
-};
+  navItems: { name: string; href: string }[];
+}
 
-const Navigation = ({className, navItems}:NavigationProps) => {
+const Navigation = ({ className, navItems }: NavigationProps) => {
   return (
     <ul className={`navigation ${className}`}>
-        {navItems.map((navigation, index) => (
-          <NavigationItem name={navigation.name} href={navigation.href} key={index}/>
-        ))}
+      {navItems.map((navigation, index) => (
+        <NavigationItem
+          name={navigation.name}
+          href={navigation.href}
+          key={index}
+        />
+      ))}
     </ul>
   );
 };
