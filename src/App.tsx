65d34805076e50227f3de1ebad4592ed6b74fb91
Korgehah <korgehah@gmail.com>
2022-02-22 12:07:33 +0300

    import images

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,36 @@ import React from 'react';
 /* scss */
 import './assets/scss/index.scss';
 /* images */
-import logo from './assets/images/logo.svg';
-import review_1 from './assets/images/review_1.png';
-import review_2 from './assets/images/review_2.png';
-import review_3 from './assets/images/review_3.png';
-import review_4 from './assets/images/review_4.png';
-import comment from './assets/images/comment.svg';
-import example from './assets/images/example.png';
-import squarespace from './assets/images/squarespace.svg';
-import ozan from './assets/images/ozan.svg';
-import padoq from './assets/images/padoq.svg';
-import stockstotrade from './assets/images/stockstotrade.svg';
-import blank from './assets/images/blank.svg';
-import feature_1 from './assets/images/feature_1.png';
-import feature_2 from './assets/images/feature_2.png';
-import feature_3 from './assets/images/feature_3.png';
-import feature_4 from './assets/images/feature_4.png';
-import rank from './assets/images/rank.svg';
-import management from './assets/images/management.png';
-import benefits_1 from './assets/images/benefits_1.svg';
-import benefits_2 from './assets/images/benefits_2.svg';
-import benefits_3 from './assets/images/benefits_3.svg';
-import benefits_4 from './assets/images/benefits_4.svg';
-import benefit_image_1 from './assets/images/benefit_image_1.png';
-import benefit_image_2 from './assets/images/benefit_image_2.png';
-import company1 from './assets/images/company1.png';
-import company2 from './assets/images/company2.png';
-import company3 from './assets/images/company3.png';
-import burger from './assets/images/burger.svg';
+import {
+  logo,
+  review_1,
+  review_2,
+  review_3,
+  review_4,
+  comment,
+  example,
+  squarespace,
+  ozan,
+  padoq,
+  stockstotrade,
+  blank,
+  feature_1,
+  feature_2,
+  feature_3,
+  feature_4,
+  rank,
+  management,
+  benefits_1,
+  benefits_2,
+  benefits_3,
+  benefits_4,
+  benefit_image_1,
+  benefit_image_2,
+  company1,
+  company2,
+  company3,
+  burger,
+} from './images/index';
 
 /* components */
 import Button from './components/Button';
